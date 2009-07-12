952cd1d6715b84dad088c3c76764a995192eda9d
Micha Niskin <asdf@ubergibson.com>
2009-07-12 14:07:19 +0000

    added safari plugin

--- init.js
+++ init.js
@@ -3,6 +3,7 @@ tinyMCE.init({
   force_root_block: false,
   mode:  "none",
   theme: "advanced",
+  plugins: "safari",
   theme_advanced_toolbar_location: "external",
   theme_advanced_buttons1: "cut,copy,paste,|,undo,redo,|,formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,outdent,indent,|,link,unlink,|,blockquote,sub,sup",
   theme_advanced_buttons2: "",
