901d44817c6b9e992f3e00f721711816cfd7fdeb
Micha Niskin <asdf@ubergibson.com>
2009-07-14 22:05:55 +0000

    removed spurious packets

--- init.js
+++ init.js
@@ -1,16 +1,14 @@
-function loadTiny() {
-  tinyMCE.init({
-    content_css: "style.css",
-    force_root_block: false,
-    mode:  "none",
-    theme: "advanced",
-    plugins: "safari",
-    theme_advanced_toolbar_location: "external",
-    theme_advanced_buttons1: "cut,copy,paste,|,undo,redo,|,formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,outdent,indent,|,link,unlink,|,blockquote,sub,sup",
-    theme_advanced_buttons2: "",
-    theme_advanced_buttons3: ""
-  });
-}
+tinyMCE.init({
+  content_css: "style.css",
+  force_root_block: false,
+  mode:  "none",
+  theme: "advanced",
+  plugins: "safari",
+  theme_advanced_toolbar_location: "external",
+  theme_advanced_buttons1: "cut,copy,paste,|,undo,redo,|,formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,outdent,indent,|,link,unlink,|,blockquote,sub,sup",
+  theme_advanced_buttons2: "",
+  theme_advanced_buttons3: ""
+});
 $(function() {
   $("head").append(
     "<style type='text/css'> "+
@@ -25,9 +23,6 @@ $(function() {
     "</style>"
   );
   $("body").append("<div id='loading'>Loading...</div>");
-
-  loadTiny();
-
   $(".eip").eip();
   $("#loading").remove();
 });
