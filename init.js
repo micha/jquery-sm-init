cd20d891887f036e60fa3a4d27a0650fd2819904
Micha Niskin <asdf@ubergibson.com>
2009-07-14 21:36:11 +0000

    loading

--- init.js
+++ init.js
@@ -1,21 +1,33 @@
-tinyMCE.init({
-  content_css: "style.css",
-  force_root_block: false,
-  mode:  "none",
-  theme: "advanced",
-  plugins: "safari",
-  theme_advanced_toolbar_location: "external",
-  theme_advanced_buttons1: "cut,copy,paste,|,undo,redo,|,formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,outdent,indent,|,link,unlink,|,blockquote,sub,sup",
-  theme_advanced_buttons2: "",
-  theme_advanced_buttons3: ""
-});
-$(function() {
-  $.eip.init({
-    "background-image" : "url('http://cf.js.simplemiami.com/js/img/stripes.gif') !important",
-    "opacity" : "0.33",
-    "filter" : "alpha(opacity = 33)",
-    "cursor" : "pointer"
+function loadTiny() {
+  tinyMCE.init({
+    content_css: "style.css",
+    force_root_block: false,
+    mode:  "none",
+    theme: "advanced",
+    plugins: "safari",
+    theme_advanced_toolbar_location: "external",
+    theme_advanced_buttons1: "cut,copy,paste,|,undo,redo,|,formatselect,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,outdent,indent,|,link,unlink,|,blockquote,sub,sup",
+    theme_advanced_buttons2: "",
+    theme_advanced_buttons3: ""
   });
+}
+$(function() {
+  $("head").append(
+    "<style type='text/css'> "+
+      "#loading { "+
+        "position: fixed; "+
+        "top: 0px; "+
+        "left: 0px; "+
+        "padding: 2px; "+
+        "color: white; "+
+        "background: red; "+
+      "} "+
+    "</style>"
+  );
+  $("body").append("<div id='loading'>Loading...</div>");
+
+  loadTiny();
+
+  $(".eip").eip();
   $("#loading").remove();
-  $("#content").show();
 });
